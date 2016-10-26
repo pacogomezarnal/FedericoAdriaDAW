5424561e54228a4f95589fecd900a12acf46f179
Federico Adria <transylv@gmail.com>
2016-10-26 19:23:55 +0200

    Falta que usemos la ruta /empresa/new en vez de nueva y solucionar el problema para mostrar una página si tenemos éxito al crear la empresa,en esta mostraremos los datos de la empresa creada

--- src/gestorBundle/Controller/empresaController.php
+++ src/gestorBundle/Controller/empresaController.php
@@ -4,6 +4,8 @@ namespace gestorBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use gestorBundle\Entity\empresa;
+use gestorBundle\Form\empresaType;
+use Symfony\Component\HttpFoundation\Request;
 
 class empresaController extends Controller
 {
@@ -19,16 +21,43 @@ class empresaController extends Controller
     {
     	//Nuevo objeto de tipo Empresa
     	$empresa = new empresa();
-    	$empresa->setNombre("ValenciaSoft");
-    	$empresa->setDireccion("Barcelona");
-    	$empresa->setCp(45699);
-    	$empresa->setTelefono1(961568874);
-    	$empresa->setTelefono2(607460445);
+    	$empresa->setNombre("Oh my PHP");
+    	$empresa->setDireccion("Valencia");
+    	$empresa->setCp(46900);
+    	$empresa->setTelefono1(934562187);
+    	$empresa->setTelefono2(604982351);
 
     	//Doctrine
     	$mangDoct=$this->getDoctrine()->getManager();
     	$mangDoct->persist($empresa);
     	$mangDoct->flush($empresa);
     	return $this->render('gestorBundle:empresa:crearEmpresa.html.twig',array("empresaId"=>$empresa->getId()));
+    }
+
+    public function nuevaEmpresaAction(Request $request)//cambiar esto por la URL que queramos
+    {
+    	$empresa=new empresa();
+    	$form= $this->createForm(empresaType::class);
+
+    	$form->handleRequest($request);
+    	if ($form->isSubmitted() && $form->isValid()) {
+    		// $form->getData() holds the submitted values
+    		// but, the original '$task' variable has also been updated
+    		$empresa = $form->getData();
+
+    		// ... perform some action, such as saving the task to the database
+    		// for example, if Task is a Doctrine entity, save it!
+    		$em = $this->getDoctrine()->getManager();
+    		$em->persist($empresa);
+    		$em->flush();
+
+    		//return $this->redirectToRoute('/empresa/msgExito');//no va
+    	}
+    	return $this->render('gestorBundle:empresa:nuevaEmpresa.html.twig',array("form"=>$form->createView() ));
+    }
+
+    public function msgExitoAction()
+    {
+        return $this->render('gestorBundle:empresa:msgExito.html.twig');
     }
 }
---
+++ src/gestorBundle/Form/empresaType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace gestorBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
+
+class empresaType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nombre')
+            ->add('direccion')
+            ->add('cp')
+            ->add('telefono1')
+            ->add('telefono2')
+            ->add('fechaCreacion')
+            ->add('Salvar',SubmitType::class)
+            ->add('Borrar',ResetType::class)
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'gestorBundle\Entity\empresa'
+        ));
+    }
+}
