f461340e499b591f26e88fa99bb9ec52e7f98b14
= <=>
2016-11-17 12:38:10 +0100

    codigo OK

--- src/gestorBundle/Controller/empresasAPIController.php
+++ src/gestorBundle/Controller/empresasAPIController.php
@@ -24,13 +24,12 @@ class empresasAPIController extends Controller
 	function generarJSONAction()
 	{
     	$repository= $this->getDoctrine()->getRepository('gestorBundle:empresa');
-    	// encontrar todos los alumnos
     	$empresas = $repository->findAll();
        $data=array('empresas'=>array());
        foreach ($empresas as $empresa) {
        	$data['empresas'][]=$this->serializeEmpresa($empresa);
        }
-       $response=new JsonResponse($data, 400);
+       $response=new JsonResponse($data, 200);
        return $response;
      }
 }
