8090676b65e5673d0f46d734c960e15bd59925b1
= <=>
2016-11-17 12:17:31 +0100

    API con GET acabada

--- src/gestorBundle/Controller/empresasAPIController.php
+++ src/gestorBundle/Controller/empresasAPIController.php
@@ -4,10 +4,23 @@ namespace gestorBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use gestorBundle\Entity\empresa;
-use Symfony\Component\Serializer\Encoder\JsonResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class empresasAPIController extends Controller
 {
+    private function serializeEmpresa(Empresa $empresa)
+    {
+      return array(
+          'nombre' => $empresa->getNombre(),
+          'direccion' => $empresa->getDireccion(),
+          'cp' => $empresa->getCp(),
+          'telefono1' => $empresa->getTelefono1(),
+          'nombre' => $empresa->getNombre(),
+          'telefono2' => $empresa->getTelefono2(),
+          'fechaCreacion' => $empresa->getFechaCreacion(),
+      );
+    }
+
 	function generarJSONAction()
 	{
     	$repository= $this->getDoctrine()->getRepository('gestorBundle:empresa');
